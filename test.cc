9c5853f67563f886cb5eb82e1b9f910291e5a4b6
geckoo <geckospaniel@gmail.com>
2022-09-18 23:50:41 +0300

    Nothing :-)

--- test.cc
+++ test.cc
@@ -1,30 +1,11 @@
 #include "akimbo/Core.hh"
-#include "akimbo/ui/Switch.hh"
-#include "akimbo/ui/Logger.hh"
-#include "akimbo/ui/Label.hh"
-#include "akimbo/ui/Button.hh"
-#include "akimbo/ui/TextInput.hh"
+#include "akimbo/ui/Console.hh"
 
 class Test : public Akimbo::Core
 {
 public:
 	Test() : texture("resources/atlas.png", 4,4)
 	{
-		auto& b = ui.add <Akimbo::UI::Logger> (
-			ui.left(10), ui.top(10),
-			ui.right(10), ui.bottom(50)
-		);
-
-		auto& l = ui.add <Akimbo::UI::TextInput> (
-			ui.left(50).then(-0.9f), b.bottom(0.1f),
-			ui.right(50).then(-0.9f), b.bottom(0.4f)
-		);
-
-		b.setRows(10);
-		l.onSubmit = [&b](const std::string& text)
-		{
-			b.addMessage(text);
-		};
 	}
 
 	void onRender(Akimbo::Render& render) override
