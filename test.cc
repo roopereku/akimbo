0e67816df5c0d091e4b4ad83c01636ec02cfe5b2
name <mail>
2022-10-22 16:21:37 +0300

    Check for mouse clicks in the most recent widgets first

--- test.cc
+++ test.cc
@@ -1,25 +1,44 @@
 #include "akimbo/Core.hh"
+#include "akimbo/Debug.hh"
 #include "akimbo/ui/Console.hh"
 #include "akimbo/ui/TabbedContainer.hh"
 #include "akimbo/ui/Switch.hh"
+#include "akimbo/ui/Widget.hh"
+
+class Overlay : public Akimbo::UI::Widget
+{
+public:
+	void onMouseClick(Vec2, int) override
+	{
+		DBG_LOG("overlay clicked");
+	}
+};
 
 class Test : public Akimbo::Core
 {
 public:
 	Test() : texture("resources/cardatlas.jpg", 13,5), p(0, 0)
 	{
+		ui.add <Akimbo::UI::Switch> (
+			ui.left(), ui.top(),
+			ui.left(25), ui.top(25)
+		);
+
+		ui.add <Akimbo::UI::Switch> (
+			ui.right(25), ui.top(),
+			ui.right(), ui.top(25)
+		);
+
+		ui.add <Overlay> (
+			ui.left(), ui.top(),
+			ui.left(50), ui.bottom()
+		);
 	}
 
 	void onRender(Akimbo::Render& render) override
 	{
 		render.color(0.5f, 0.5f, 0.5f);
 		render.clear();
-
-		Vec2 size(0.5f, 0.5f);
-
-		render.color(1.0f, 1.0f, 1.0f);
-		render.texture(texture, render.center, render.radius);
-		render.fromAtlas(texture, 0, 1, render.topLeft, size * texture.getAspectRatio());
 	}
 
 private:
