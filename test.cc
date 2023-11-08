1abd36ef5e2a4876d05b10037c212e6a94df4d7e
roopereku <roopereku@gmail.com>
2023-11-08 23:43:41 +0200

    Initial work on UI system

--- test.cc
+++ test.cc
@@ -1,34 +1,8 @@
 #include <akimbo/Core.hh>
 #include <akimbo/ValueProperty.hh>
 #include <akimbo/Main.hh>
-
 #include <akimbo/SDL2/Window.hh>
-
-class TestContent : public akimbo::WindowContent
-{
-public:
-	static TestContent& add()
-	{
-		return getCore().addUpdating(new TestContent);
-	}
-
-private:
-	void onRender(akimbo::Renderer2D& renderer) override
-	{
-		renderer.clear(r, 0.0f, 0.0f);
-	}
-
-	void onUpdate() override
-	{
-		c += 0.01f;
-		r = (sin(c) + 1) / 2;
-
-		render();
-	}
-
-	float c = 0.0f;
-	float r = 0.0f;
-};
+#include <akimbo/ui/Root.hh>
 
 class Test : public akimbo::Main
 {
@@ -38,7 +12,7 @@ public:
 		auto& window = akimbo::SDL2::Window::add();
 		window.createRenderer();
 
-		auto& content = TestContent::add();
+		auto& ui = akimbo::UI::Root::add();
 		window.content = content;
 	}
 
